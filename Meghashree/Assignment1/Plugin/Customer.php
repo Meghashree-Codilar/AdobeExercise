44a97e2ab96455f17dbbafbbb7cfbfbb6d1824d2
Meghashree-Codilar <94516536+Meghashree-Codilar@users.noreply.github.com>
2022-06-29 23:03:41 +0530

    Assignment1-Training

---
+++ Meghashree/Assignment1/Plugin/Customer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Meghashree\Assignment1\Plugin;
+
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Customer\Model\Data\Customer as Subject;
+
+class Customer
+{
+    /**
+     * @var DateTime $dateTime
+     */
+    public DateTime $dateTime;
+
+    /**
+     * @param DateTime $dateTime
+     * @return void
+     */
+    public function __construct(DateTime $dateTime)
+    {
+        $this->dateTime=$dateTime;
+    }
+
+    /**
+     * After Get Method
+     *
+     * @param Subject $subject
+     * @param $middlename
+     * @return string
+     */
+    public function afterGetMiddlename(Subject $subject, $middlename)
+    {
+        $dob=$subject->getDob();
+        $current = time();
+        $birth_date = $this->dateTime->gmtTimestamp($dob);
+        $diff = $current - $birth_date;
+        $result=(int)($diff / (60 * 60 * 24));
+        return (int)($result/365);
+
+    }
+}
